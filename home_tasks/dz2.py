76f0a3a5dd60b62382fd6b11da7ece38b5d4bc41
marina ursu <95243711+marinaursu@users.noreply.github.com>
2021-12-11 17:15:33 +0300

    Update dz2.py

--- home_tasks/dz2.py
+++ home_tasks/dz2.py
@@ -1,12 +1,12 @@
 name = input("введите свое имя : ")
 age = int(input("укажите ващ возраст : "))
-agelist = [" годик"," года"," лет"]
+agelist = [" год"," года"," лет"]
 age1 = 0
-if age == 1:
+age_split = int(round((age/10 % 1),1) * 10) # возраст делим на 10, потом берем десятичную часть, округляем ее до одного знака после запятой, умножаем на 10 и приобразуем из float в int
+if age != 11 and age_split == 1:
     age1 = 0
-elif age in range(2,4):
+elif age != 11 and age_split in range(2,4):
     age1 = 1
 else:
     age1 = 2
-
 print("Привет " + name + ", тебе уже " + str(age) + agelist[age1] + " это здорово! ")
